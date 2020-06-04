dab6b850a9f96d8324b16ae85eac9cda0e1ed101
figux <figux01@gmail.com>
2020-06-04 23:16:36 +0000

    ok

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    var connection = new WebSocket('ws://127.0.0.1:8080');
+
+    connection.onopen = function () {
+        // connection is opened and ready to use
+        console.log("connection is opened and ready to use");
+
+        connection.send("text");
+    };
+
+    connection.onerror = function (error) {
+        // an error occurred when sending/receiving data
+        console.log("an error occurred when sending/receiving data");
+    };
+
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message
+        // from server is json)
+        console.log(message);
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+        console.log('This doesn\'t look like a valid JSON: ',
+            message.data);
+        return;
+        }
+        // handle incoming message
+    };
+
+});
---
+++ server.js
@@ -0,0 +1,57 @@
+const WebSocket = require('ws');
+var http = require('http');
+
+const wss = new WebSocket.Server({
+    port: 8080,
+    host: '127.0.0.1',
+    perMessageDeflate: {
+      zlibDeflateOptions: {
+        // See zlib defaults.
+        chunkSize: 1024,
+        memLevel: 7,
+        level: 3
+      },
+      zlibInflateOptions: {
+        chunkSize: 10 * 1024
+      },
+      // Other options settable:
+      clientNoContextTakeover: true, // Defaults to negotiated value.
+      serverNoContextTakeover: true, // Defaults to negotiated value.
+      serverMaxWindowBits: 10, // Defaults to negotiated value.
+      // Below options specified as default values.
+      concurrencyLimit: 10, // Limits zlib concurrency for perf.
+      threshold: 1024 // Size (in bytes) below which messages
+      // should not be compressed.
+    }
+  });
+wss.on('connection',function(ws, req){
+    console.log('started client interval');
+
+    console.log
+
+    ws.on('close', function() {
+        console.log('stopping client interval');
+        //clearInterval(id);
+    });
+
+
+    setTimeout(function(){
+        ws.send("datafromServeur");
+    },10000)
+
+    ws.on('message', function(data){
+        //{'type':'offer','action':'AskclientTopatient or AskpatientToClient','sdp':''}
+        console.log(data);
+        //var dataJson  = JSON.parse(data);
+        ws.send(data);
+
+    });
+});
+
+
+var server = http.createServer(function(request, response) {
+    
+  });
+server.listen(8080, function() {
+    console.log("serveur created");
+ });

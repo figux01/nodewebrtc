dc787c919b246428690f4040af1a46a4f9a1a693
figux <figux01@gmail.com>
2020-06-05 02:45:46 +0000

    ok

---
+++ index2.js
@@ -0,0 +1,14 @@
+var evtSource = new EventSource("e-liveteaching.com/webrtc/index.php");
+evtSource.onmessage = function(e) {
+    var newElement = document.createElement("li");
+
+    newElement.innerHTML = "message: " + e.data;
+    eventList.appendChild(newElement);
+}
+evtSource.addEventListener("ping", function(e) {
+    var newElement = document.createElement("li");
+
+    var obj = JSON.parse(e.data);
+    newElement.innerHTML = "ping at " + obj.time;
+    eventList.appendChild(newElement);
+}, false);
